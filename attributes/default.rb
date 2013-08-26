542e763c3f2a3e3def16cecb889e92b33d37d930
Adrian Hornsby <adrian@hdmessaging.com>
2013-08-26 17:14:08 +0300

    adding centos package names - more

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,20 +24,20 @@ default[:ffmpeg][:prefix] = "/usr/local"
 default[:ffmpeg][:git_repository] = "git://git.videolan.org/ffmpeg.git"
 default[:ffmpeg][:compile_flags] = [
   "--disable-debug",
+  "--enable-version3",
   "--enable-pthreads",
   "--enable-nonfree",
   "--enable-gpl",
-  "--disable-indev=jack",
+  "--disable-ffplay",
   "--enable-libx264",
   "--enable-libfaac",
   "--enable-libmp3lame",
   "--enable-libtheora",
   "--enable-libvorbis",
   "--enable-libvpx",
-  "--enable-libxvid",
-  "--enable-libfaad"
+  "--enable-libxvid"
 ]
 
 # JW 07-06-11: Hash of commit or a HEAD should be used - not a tag. Sync action of Git
 # provider will always attempt to update the git clone if a tag is used.
-default[:ffmpeg][:git_revision]   = "ace432f62cdcedf812e7c4d77fc5b03322170fa8" # 0.6.3
+default[:ffmpeg][:git_revision]   = "e820e3a2591a0d544925ff1522d2a688a647f1b0" # 0.6.3
